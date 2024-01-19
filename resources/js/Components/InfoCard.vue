7a232ba75a320d63318b9439fd6d64ff85d8b91e
Tene Tiitma <tenetiitma@gmail.com>
2024-01-19 12:01:13 +0200

    fixed cards

--- resources/js/Components/InfoCard.vue
+++ resources/js/Components/InfoCard.vue
@@ -12,25 +12,27 @@ defineProps({
   >
     <img :src="item.img" />
     <div class="p-2">
-      <p class="text-2xl font-bold">{{ item.activity }}</p>
-      <div class="flex">
+      <p class="text-xl font-bold mb-2">{{ item.activity }}</p>
+      <div class="flex mb-2">
         <StarIcon class="w-6 h-6 text-yellow-400 mr-1" />
         <p class="text-xl text-yellow-400">{{ item.rating }}</p>
       </div>
 
-      <p>{{ item.location }}</p>
-      <p v-if="item.discounted_price" class="text-xl font-bold text-rose-600">
-        {{ item.discounted_price }} EUR
-      </p>
-      <p
-        :class="[
-          item.discounted_price
-            ? 'text-xl font-bold line-through'
-            : 'text-xl font-bold',
-        ]"
-      >
-        {{ item.full_price }} EUR
-      </p>
+      <p class="mb-3">{{ item.location }}</p>
+      <div class="grid grid-cols-2">
+        <p v-if="item.discounted_price" class="text-xl font-bold text-rose-600">
+          {{ item.discounted_price }} EUR
+        </p>
+        <p
+          :class="[
+            item.discounted_price
+              ? 'text-sm font-bold line-through text-end'
+              : 'text-xl font-bold',
+          ]"
+        >
+          {{ item.full_price }} EUR
+        </p>
+      </div>
     </div>
   </div>
 </template>
