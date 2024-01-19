01d096844b6e830758d8e6303d28c3940f6a3851
Tene Tiitma <tenetiitma@gmail.com>
2024-01-19 11:50:49 +0200

    Rating fixed

--- resources/js/Components/InfoCard.vue
+++ resources/js/Components/InfoCard.vue
@@ -1,21 +1,36 @@
-<template>
-  <div class="pb-4 bg-white border-2 border-black">
-    <img :src="item.img">
-    <div class="p-2">
-    <p class="text-2xl font-bold">{{ item.activity }}</p>
-    <p class="text-xl text-yellow-400">{{ item.rating }}</p>
-    <p>{{ item.location }}</p>
+<script setup>
+import { StarIcon } from "@heroicons/vue/24/solid";
 
-    <p v-if="item.discounted_price" class="text-xl font-bold text-rose-600">{{ item.discounted_price }} EUR</p>
-    <p :class='[item.discounted_price ? "text-xl font-bold line-through": "text-xl font-bold"]'>{{ item.full_price }} EUR</p>
-  </div>
+defineProps({
+  item: Object,
+});
+</script>
+
+<template>
+  <div
+    class="pb-4 bg-white border-[1px] shadow-md hover:scale-[1.1] ease-in-out duration-300 p-1"
+  >
+    <img :src="item.img" />
+    <div class="p-2">
+      <p class="text-2xl font-bold">{{ item.activity }}</p>
+      <div class="flex">
+        <StarIcon class="w-6 h-6 text-yellow-400 mr-1" />
+        <p class="text-xl text-yellow-400">{{ item.rating }}</p>
+      </div>
+
+      <p>{{ item.location }}</p>
+      <p v-if="item.discounted_price" class="text-xl font-bold text-rose-600">
+        {{ item.discounted_price }} EUR
+      </p>
+      <p
+        :class="[
+          item.discounted_price
+            ? 'text-xl font-bold line-through'
+            : 'text-xl font-bold',
+        ]"
+      >
+        {{ item.full_price }} EUR
+      </p>
+    </div>
   </div>
 </template>
-
-<script>
-export default {
-  props: {
-    item: Object,
-  },
-};
-</script>
