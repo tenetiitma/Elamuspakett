8aae1c9e6961deafcac04a230fb94b669c9f630e
Aren Ansper <aren.ansper@gmail.com>
2024-01-19 12:40:36 +0200

    pakkettt

--- resources/js/Pages/Packet.vue
+++ resources/js/Pages/Packet.vue
@@ -9,22 +9,30 @@ import GuestLayout from "@/Layouts/GuestLayout.vue";
         ELAMUSTEENUSED
       </p>
     </template>
-    <div>
-      <h3 class="font-bold text-center flex">OUTDOOR ADVENTURE</h3>
+    <div class="flex flex-col items-center">
+      <p class="text-5xl font-bold">OUTDOOR ADVENTURE</p>
       <p class="font-bold">Mountain peak expedition</p>
-      <div class="max-w-7xl max-h-96">
-        <img
+      <div class="flex flex-row w-[580px]">
+        <img class="border-2 rounded-lg"
           src="https://cdn.kingitus.ee/storage/photos/products/105749/17636.jpg?w=2000&h=2000"
           alt="saun"
         />
+        <img class="border-2 rounded-lg"
+          src="https://cdn.kingitus.ee/storage/photos/products/020402/7891.jpg?w=2000&h=2000"
+          alt="langevari"
+        />
+        <img class="border-2 rounded-lg"
+          src="https://wwwnc.cdc.gov/travel/images/mountain-biker.jpg"
+          alt="langevari"
+        />
       </div>
-      <ul>
-        <li>€</li>
-        <li>Asukoht:</li>
-        <li>Riietus:</li>
-        <li>Osalejad:</li>
-        <li>Kestvus:</li>
-        <li>Lisainfo:</li>
+      <ul class="pb-4">
+        <li class="text-2xl flex flex-row gap-4">Hind:<p class="text-2xl font-bold text-rose-600">120</p>€</li>
+        <li class="text-2xl flex flex-row gap-4">Asukoht:<p class="text-2xl font-bold">Karujärve Saaremaa</p></li>
+        <li class="text-2xl flex flex-row gap-4">Riietus:<p class="text-2xl font-bold">Vastavalt ilmale</p></li>
+        <li class="text-2xl flex flex-row gap-4">Osalejad:<p class="text-2xl font-bold">2-4</p>inimest</li>
+        <li class="text-2xl flex flex-row gap-4">Kestvus:<p class="text-2xl font-bold">120</p>min</li>
+        <li class="text-2xl flex flex-row gap-4">Lisainfo:<p class="text-m font-bold"></p></li>
       </ul>
     </div>
   </GuestLayout>
